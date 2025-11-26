4e151174aa1b78932ade8d201f3a5bc55ab354ec
huanghao <huanghao@youxin.cloud>
2025-11-26 11:32:42 +0800

    Chinese i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
+import { ConfigProvider } from 'antd';
 import "antd/dist/reset.css";
+import zhCN from 'antd/locale/zh_CN';
+import 'dayjs/locale/zh-cn';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router";
@@ -10,14 +13,16 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/demo1" element={<Demo1 />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider locale={zhCN}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="demo1" element={<Demo1 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   </StrictMode>,
 )
