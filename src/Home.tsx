a627a1b7f0c291e2e9ec1f9aa816aed77c680991
huanghao <huanghao@youxin.cloud>
2025-11-27 17:00:17 +0800

    Add Product List

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,9 @@
-import { readMe, serverInfo } from "@directus/sdk";
+import { readItems, readMe, serverInfo } from "@directus/sdk";
 import { useRequest } from "ahooks";
-import { Card, Flex } from "antd";
-import { useDirectus } from "./directus";
+import { Avatar, Card, Flex } from "antd";
+import { useNavigate } from "react-router";
+import { useDirectus, useDirectusAuth } from "./directus";
+import { asset } from "./directus/assets";
 
 
 function ServerInfo() {
@@ -28,10 +30,74 @@ function UserInfo() {
     )
 }
 
+function ProductList() {
+    const navigate = useNavigate()
+
+    const [directus, token] = useDirectusAuth()
+
+    const { data } = useRequest(async () => {
+        return await directus.request(readItems('product', {
+            fields: [
+                'id',
+                'status',
+                'name',
+                'description',
+                'brand_id.name',
+                'brand_id.image',
+                'images.directus_files_id',
+                'categories.category_id.id',
+                'categories.category_id.name'
+            ]
+        }))
+    })
+
+    if (!token) {
+        return (<></>)
+    }
+
+    const onClickCard = (productId: string) => {
+        navigate(`/product/${productId}`)
+    }
+
+    return (
+        <Flex wrap gap="large">
+            {data?.map((product) => (
+                <Card
+                    onClick={() => onClickCard(product.id)}
+                    key={product.id}
+                    hoverable
+                    style={{ width: 300 }}
+                    cover={
+                        <img
+                            draggable={false}
+                            alt="image"
+                            src={asset(directus, product.images[0]?.directus_files_id, { token })}
+                        />
+                    }
+                >
+                    <Card.Meta
+                        avatar={<Avatar src={asset(directus, product.brand_id?.image, { token })} />}
+                        title={product.name}
+                        description={product.description}
+                    />
+                    <Flex vertical style={{ marginTop: 20 }}>
+                        <div>{product.status}</div>
+                        {/*@ts-expect-error category is any type*/}
+                        {product.categories.map((category) => (
+                            <div key={category.category_id.id}>{category.category_id.name}</div>
+                        ))}
+                    </Flex>
+                </Card>
+            ))}
+        </Flex>
+    )
+}
+
 export function Home() {
     return (
         <Flex vertical gap="large">
             <div>首页</div>
+            <ProductList />
             <ServerInfo />
             <UserInfo />
         </Flex>
---
+++ src/directus/assets.ts
@@ -0,0 +1,36 @@
+import type { AssetsQuery } from '@directus/sdk';
+import { useDirectus } from '.';
+
+type AssetOptions = Extract<AssetsQuery, { key?: never }> & {
+    token?: string;
+};
+
+export function asset(
+    directus: ReturnType<typeof useDirectus>,
+    fileId: string,
+    options: AssetOptions = {},
+) {
+    const assetURL = new URL(`/assets/${fileId}`, directus.url)
+    const searchParams = assetURL.searchParams
+
+    if (options.format) {
+        searchParams.set('format', options.format);
+    }
+    if (options.width) {
+        searchParams.set('width', String(options.width));
+    }
+    if (options.height) {
+        searchParams.set('height', String(options.height));
+    }
+    if (options.quality) {
+        searchParams.set('quality', String(options.quality));
+    }
+    if (options.fit) {
+        searchParams.set('fit', options.fit);
+    }
+    if (options.token) {
+        searchParams.set('access_token', options.token);
+    }
+
+    return assetURL.toString();
+}
