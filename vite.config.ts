48950a9215f91bddedd8e0f3e8dec67e332bb893
huanghao <huanghao@youxin.cloud>
2025-11-27 09:35:45 +0800

    Improve chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 900,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router'],
+          antd: ['antd'],
+          ahooks: ['ahooks']
+        }
+      }
+    }
+  }
 })
