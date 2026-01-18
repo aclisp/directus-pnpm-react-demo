9bdbbc5cec03cc7af1ae9f6ae52f6844d355bfc5
huanghao <huanghao@youxin.cloud>
2026-01-18 17:46:00 +0800

    Add Pagination to LookupSelect

--- src/components/LookupSelect.tsx
+++ src/components/LookupSelect.tsx
@@ -1,8 +1,8 @@
 import { useDirectus } from '@/directus'
 import { aggregate, readItem, readItems } from '@directus/sdk'
-import { useDebounce, useRequest } from 'ahooks'
-import type { TableColumnsType, TableProps } from 'antd'
-import { Flex, Input, Modal, Select, Table, theme } from 'antd'
+import { useDebounce, useRequest, useUpdateEffect } from 'ahooks'
+import type { PaginationProps, TableColumnsType, TableProps } from 'antd'
+import { Flex, Input, Modal, Pagination, Select, Table, theme } from 'antd'
 import { useEffect, useState } from 'react'
 import { ImageRender } from './ImageRender'
 import type { CollectionField, Item, Item2 } from './types'
@@ -139,6 +139,10 @@ function LookupSelectionModal({
     const [selectionList, setSelectionList] = useState<LookupSelectValueType[]>([])
     const [selection, setSelection] = useState<SelectionType>(() => valueToSelection(currentValue))
 
+    const [currentPage, setCurrentPage] = useState(1)
+    const [pageSize, setPageSize] = useState(10)
+    const [totalCount, setTotalCount] = useState<number>()
+
     // Columns are derived from props
     const columns: TableColumnsType<LookupSelectValueType> = collectionFields.map((x) => {
         const column = {
@@ -156,29 +160,13 @@ function LookupSelectionModal({
     // Ensure that the id field is always included
     const fields = columns.map(x => String(x.key)).concat('id')
 
-    // Limit the count of directus readItems request
-    const limit = 10
-
-    const {
-        showSearch,
-        setShowSearch,
-        searchValue,
-        setSearchValue,
-    } = useSearchRequest({
-        collection,
-        fields,
-        filter,
-        limit,
-        onSuccess: data => setSelectionList(data as LookupSelectValueType[]),
-    })
-
     // Effect 1: Synchronize selection state when the external value changes
     useEffect(() => {
         setSelection(valueToSelection(currentValue))
     }, [currentValue])
 
     // Effect 2: Fetch data when the modal opens
-    const { run } = useRequest(async () => {
+    const { run: runPageRequest, loading } = useRequest(async () => {
         // If there's initialValue, then use it and exclude others.
         if (initialValue) {
             const data = await directus.request(readItem(collection, initialValue.id, { fields }))
@@ -189,35 +177,69 @@ function LookupSelectionModal({
         const dataList = await directus.request(readItems(collection, {
             fields,
             filter,
-            limit,
+            limit: pageSize,
+            page: currentPage,
         }))
 
         // If there's currentValue, then get it and merge it into the final result.
-        if (currentValue) {
-            const data = await directus.request(readItem(collection, currentValue.id, { fields }))
-            if (dataList.find(v => v.id === data.id) === undefined) {
-                dataList.unshift(data)
-                dataList.pop()
-            }
-        }
-
-        // Determine if 'Search' shoud be present
-        directus.request(aggregate(collection, {
-            aggregate: { count: '*' },
-            query: { filter },
-        })).then((data) => {
-            if (Number(data[0]?.count) > limit) {
-                setShowSearch(true)
-            }
-        })
+        // if (currentValue) {
+        //     const data = await directus.request(readItem(collection, currentValue.id, { fields }))
+        //     if (dataList.find(v => v.id === data.id) === undefined) {
+        //         dataList.unshift(data)
+        //         dataList.pop()
+        //     }
+        // }
 
         return dataList
     }, {
         onSuccess: data => setSelectionList(data as LookupSelectValueType[]),
         manual: true,
     })
+
+    useUpdateEffect(() => {
+        runPageRequest()
+    }, [currentPage, pageSize])
+
+    const {
+        showSearch,
+        setShowSearch,
+        searchValue,
+        setSearchValue,
+        searching,
+    } = useSearchRequest({
+        collection,
+        fields,
+        filter,
+        limit: 10,
+        onSuccess: data => setSelectionList(data as LookupSelectValueType[]),
+        runPageRequest,
+    })
+
+    // Fetch total count and determine if 'Search' shoud be present
+    const { run: runAggregateRequest } = useRequest(async () => {
+        if (initialValue) {
+            return []
+        }
+        return await directus.request(aggregate(collection, {
+            aggregate: { count: '*' },
+            query: { filter },
+        }))
+    }, {
+        onSuccess: (data) => {
+            const count = Number(data[0]?.count)
+            setTotalCount(count)
+            if (count > 10) {
+                setShowSearch(true)
+            }
+        },
+        manual: true,
+    })
+
     useEffect(() => {
-        if (open) run()
+        if (open) {
+            runPageRequest()
+            runAggregateRequest()
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [open])
 
@@ -251,6 +273,11 @@ function LookupSelectionModal({
         setSelection({ keys: [record.id], values: [record] })
     }
 
+    const handlePageChange: PaginationProps['onChange'] = (page, pageSize) => {
+        setCurrentPage(page)
+        setPageSize(pageSize)
+    }
+
     return (
         <Modal
             closable={false}
@@ -260,7 +287,9 @@ function LookupSelectionModal({
         >
             <Flex vertical gap="small">
                 {showSearch && (
-                    <Input
+                    <Input.Search
+                        allowClear
+                        loading={searching}
                         placeholder="搜索..."
                         value={searchValue}
                         onChange={e => setSearchValue(e.target.value)}
@@ -268,6 +297,7 @@ function LookupSelectionModal({
                 )}
                 <Table<LookupSelectValueType>
                     rowKey="id"
+                    loading={loading}
                     dataSource={selectionList}
                     columns={columns}
                     rowSelection={rowSelection}
@@ -277,7 +307,22 @@ function LookupSelectionModal({
                     pagination={false}
                     size="small"
                 />
-                {showSearch && <SearchHint />}
+                {showSearch && (
+                    <Flex wrap justify="space-between" align="center">
+                        {!searchValue
+                            && (
+                                <Pagination
+                                    showTotal={total => `共 ${total} 条`}
+                                    simple={{ readOnly: true }}
+                                    current={currentPage}
+                                    pageSize={pageSize}
+                                    total={totalCount}
+                                    onChange={handlePageChange}
+                                />
+                            )}
+                        <SearchHint />
+                    </Flex>
+                )}
             </Flex>
         </Modal>
     )
@@ -289,19 +334,21 @@ function useSearchRequest({
     filter,
     limit,
     onSuccess,
+    runPageRequest,
 }: {
     collection: string
     fields: string[]
     filter?: Record<string, unknown>
     limit: number
     onSuccess: (data: Item[]) => void
+    runPageRequest: () => void
 }) {
     const directus = useDirectus()
     const [showSearch, setShowSearch] = useState(false)
     const [searchValue, setSearchValue] = useState('')
 
     const debouncedSearchTerm = useDebounce(searchValue.trim(), { wait: 500 })
-    const { run: runSearchRequest } = useRequest(async () => {
+    const { run: runSearchRequest, loading: searching } = useRequest(async () => {
         return await directus.request(readItems(
             collection,
             {
@@ -315,11 +362,12 @@ function useSearchRequest({
         manual: true,
         onSuccess,
     })
-    useEffect(() => {
+    useUpdateEffect(() => {
         if (debouncedSearchTerm) {
             runSearchRequest()
+        } else {
+            runPageRequest()
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [debouncedSearchTerm])
 
     return {
@@ -327,6 +375,7 @@ function useSearchRequest({
         setShowSearch,
         searchValue,
         setSearchValue,
+        searching,
     }
 }
 
